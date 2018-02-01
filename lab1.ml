3b93ffc069e417ee86a1f7584330e016a018e780
Ben Kaplan <bkaplan@college.harvard.edu>
2018-02-01 16:01:06 -0500

    end of lab thru exercise 9

--- lab1.ml
+++ lab1.ml
@@ -99,7 +99,7 @@ appropriate OCaml expression to assign the value to the variable
 exercise1 below.
 ......................................................................*)
 
-let exercise3 () = failwith "exercise3 not implemented" ;;
+let exercise3 () = -(5 - 3) ;;
 
 (* Hint: The OCaml concrete expression
 
@@ -111,6 +111,19 @@ does *not* correspond to the abstract syntax above.
 Exercise 4: Draw the tree that the concrete syntax "- 5 - 3" does
 correspond to. Check it with a member of the course staff if you'd
 like.
+
+    -
+    |
+    |
+    ^
+   / \
+  /   \
+ -     3
+ |
+ |
+ 5
+
+
 ......................................................................*)
 
    
@@ -124,24 +137,24 @@ expressions below? Test your solution by uncommenting the examples
 error is generated.
 ......................................................................*)
 
-(*   <--- remove this start of comment line
 
-let exercise5a : ??? = 42 ;;
 
-let exercise5b : ??? =
+let exercise5a : int = 42 ;;
+
+let exercise5b : string =
   let greet y = "Hello " ^ y
   in greet "World!";;
 
-let exercise5c : ???  =
+let exercise5c : int * float -> int =
   fun (x, y) -> x + int_of_float y ;;
 
-let exercise5d : ??? =
+let exercise5d : int -> bool =
   fun x -> x < x + 1 ;;
 
-let exercise5e : ??? =
+let exercise5e : bool -> bool list =
   fun x -> if x then [x] else [] ;;
 
-remove this end of comment line too ----> *)
+
 
 (*======================================================================
 Part 3: First-order functional programming
@@ -170,18 +183,22 @@ to the list containing the elements 3, 4, and 5? You'll want to
 replace the "[]" with the correct functional call.
 ......................................................................*)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all (lst : int list) : int list =
+  match lst with
+  | [] -> []
+  | hd :: tl -> hd * hd :: square_all tl ;;
 
-let exercise6 = [] ;;
+let exercise6 = square_all[3; 4; 5] ;;
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
 list. (What's a sensible return value for the empty list?)
 ......................................................................*)
 
-let sum (lst : int list) : int =
-  failwith "sum not implemented" ;;
+let rec sum (lst : int list) : int =
+  match lst with
+  | [] -> 0
+  | hd :: tl -> hd + sum tl ;;
   
 (*......................................................................
 Exercise 8: Define a recursive function that returns the maximum
@@ -190,8 +207,11 @@ can raise an appropriate exception -- a Match_failure or
 Invalid_argument exception for instance.
 ......................................................................*)
 
-let max_list (lst : int list) : int =
-  failwith "max_list not implemented" ;;
+let rec max_list (lst : int list) : int =
+  match lst with
+  | [] -> raise (Invalid_argument "Empty List")
+  | [x] -> x
+  | x1 :: x2 :: tl -> if x1 > x2 then max_list (x1 :: tl) else max_list (x2 :: tl) ;;
 
 (*......................................................................
 Exercise 9: Define a function zip, that takes two int lists and
@@ -205,8 +225,13 @@ length lists, to just pad the shorter list with, say, false values, so
 that, zip [1] [2; 3; 4] = [(1, 2); (false, 3); (false, 4)]?
 ......................................................................*)
 
-let zip (x : int list) (y : int list) : (int * int) list =
-  failwith "zip not implemented" ;;
+let rec zip (x : int list) (y : int list) : (int * int) list =
+  match (x, y) with
+  | ([], []) -> []
+  | (x, []) -> raise (Invalid_argument "List 2 is too short")
+  | ([], y) -> raise (Invalid_argument "List 1 is too short")
+  | (hx :: tx, hy :: ty) -> (hx, hy) :: zip tx ty ;;
+
 
 (*.....................................................................
 Exercise 10: Recall the definition of the function prods from lecture
